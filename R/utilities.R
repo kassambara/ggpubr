62025aa6cb6a1f72dac43d6c26afa9103739e0ad
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-02-16 05:47:47 +0100

    .ggcolor and .ggfill added

--- R/utilities.R
+++ R/utilities.R
@@ -158,14 +158,14 @@ NULL
     palette <- ""
   if (length(palette) == 1) {
     if (palette %in% brewerpal)
-      ggplot2::scale_color_brewer(palette = palette)
+      ggplot2::scale_color_brewer(..., palette = palette)
     else if (palette == "grey")
-       ggplot2::scale_color_grey(start = 0.8, end = 0.2)
+       ggplot2::scale_color_grey(..., start = 0.8, end = 0.2)
     else if (palette == "hue")
       ggplot2::scale_color_hue(...)
   }
   else if (palette[1] != "")
-    ggplot2::scale_color_manual(values = palette, ...)
+    ggplot2::scale_color_manual(..., values = palette)
 }
 
 # Change fill color manually
@@ -187,14 +187,14 @@ NULL
     palette <- ""
   if (length(palette) == 1) {
     if (palette %in% brewerpal)
-      ggplot2::scale_fill_brewer(palette = palette)
+      ggplot2::scale_fill_brewer(..., palette = palette)
     else if (palette == "grey")
-      ggplot2::scale_fill_grey(start = 0.8, end = 0.2)
+      ggplot2::scale_fill_grey(..., start = 0.8, end = 0.2)
     else if (palette == "hue")
       ggplot2::scale_fill_hue(...)
   }
   else if (palette[1] != "")
-    ggplot2::scale_fill_manual(values = palette, ...)
+    ggplot2::scale_fill_manual(..., values = palette)
 }
 
 
