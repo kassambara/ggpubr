75b308101fdc849598b963b07ada012e6cbb6314
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-09-25 19:36:02 +0200

    checkpoint for size added

--- R/ggline.R
+++ R/ggline.R
@@ -180,7 +180,7 @@ ggline <- function(data, x, y, group = 1,
     p <- p +
     .geom_exec(geom_point, data = data_sum,
                color = color, shape = shape,
-               size = 2+size)
+               size = 2+ifelse(is.null(size), 0.5, 2+size))
   }
 
 
