314ad41447a35f3ac87463b0bc69f03411d21892
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-11-09 13:09:09 +0100

    updated

--- R/utilities.R
+++ R/utilities.R
@@ -11,68 +11,7 @@ NULL
 # return a plot if geomfunc!=Null or a list(option, mapping) if  geomfunc = NULL
 .geom_exec <- function (geomfunc = NULL, data = NULL,
                         position = NULL, ...) {
-  params <- list(...)
-
-  mapping <-
-    list() # option to pass to mapping aes() or aes_string()
-  option <- list() # option to the geom_*()
-
-  allowed_options <- c(
-    # general
-    "x", "y", "color", "colour", "linetype", "fill", "size", "shape", "width",
-    "alpha", "na.rm", "lwd", "pch", "cex", "position", "stat", "geom",
-    "show.legend", "inherit.aes", "fun.args", "fontface",
-    # boxplot
-    "outlier.colour", "outlier.shape", "outlier.size",
-    "outlier.stroke", "notch", "notchwidth", "varwidth",
-    # dot plot
-    "binwidth", "binaxis", "method", "binpositions",
-    "stackdir", "stackratio", "dotsize",
-    # Violin
-    "trim", "draw_quantiles", "scale",
-    # error
-    "ymin", "ymax", "xmin", "xmax",
-    # text
-    "label", "hjust", "vjust", "fontface",
-    # smooth
-    "se", "level", "fullrange",
-    "conf.int.level",
-    # straightline
-    "xintercept", "yintercept",
-    # histograms
-    "bins",
-    # rug
-    "sides",
-    # segment
-    "arrow", "xend", "yend"
-
-  )
-
-  columns <- colnames(data)
-  for (key in names(params)) {
-    value <- params[[key]]
-    if (is.null(value)) {
-
-    }
-    else if (unlist(value)[1] %in% columns) {
-      mapping[[key]] <- value
-
-    }
-  else if (key %in% allowed_options) {
-      option[[key]] <- value
-    }
-    # else warnings("Don't know '", key, "'")
-  }
-  if (!is.null(position))
-  option[["position"]] <- position
-  option[["data"]] <- data
-  if(is.null(geomfunc)){
-    return(list(option = option, mapping = mapping))
-  }
-  else{
-  option[["mapping"]] <- do.call(ggplot2::aes_string, mapping)
-  return(do.call(geomfunc, option))
-  }
+  geom_exec(geomfunc = geomfunc, data = data, position = position, ...)
 }
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -151,10 +90,8 @@ NULL
 # Graphical parameters
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Change color manually
-# possible value for palette: brewer palette, "grey" or a vector of colors
-.ggcolor <- function(palette = NULL, ...) {
-  brewerpal <- c(
+.brewerpal <- function(){
+  c(
     # sequential
     'Blues', 'BuGn', 'BuPu', 'GnBu', 'Greens', 'Greys', 'Oranges',
     'OrRd', 'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu', 'Reds',
@@ -164,11 +101,25 @@ NULL
     # Qualitative
     'Accent', 'Dark2', 'Paired', 'Pastel1', 'Pastel2', 'Set1', 'Set2', 'Set3'
   )
-
+}
+.ggscipal <- function(){
   # Scientific Journal and Sci-Fi Themed Color Palettes for ggplot2
   # ggsci package: https://cran.r-project.org/web/packages/ggsci/vignettes/ggsci.html
-  ggscipal <- c("npg", "aaas", "lancet", "jco",
+  c("npg", "aaas", "lancet", "jco",
                 "ucscgb", "uchicago", "simpsons", "rickandmorty")
+}
+
+# Check if color palette
+.is_col_palette <- function(pal){
+  if(is.null(pal)) return(FALSE)
+  else return(length(pal)==1 & pal[1] %in% c(.brewerpal(), .ggscipal()))
+}
+
+# Change color manually
+# possible value for palette: brewer palette, "grey" or a vector of colors
+.ggcolor <- function(palette = NULL, ...) {
+  brewerpal <- .brewerpal()
+  ggscipal <- .ggscipal()
 
   res <- NULL
   if (is.null(palette))
@@ -190,21 +141,8 @@ NULL
 # Change fill color manually
 # possible value for palette: brewer palette, "grey" or a vector of colors
 .ggfill <- function(palette = NULL, ...) {
-  brewerpal <- c(
-    # sequential
-    'Blues', 'BuGn', 'BuPu', 'GnBu', 'Greens', 'Greys', 'Oranges',
-    'OrRd', 'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu', 'Reds',
-    'YlGn', 'YlGnBu YlOrBr', 'YlOrRd',
-    #Divergent
-    'BrBG', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
-    # Qualitative
-    'Accent', 'Dark2', 'Paired', 'Pastel1', 'Pastel2', 'Set1', 'Set2', 'Set3'
-  )
-
-  # Scientific Journal and Sci-Fi Themed Color Palettes for ggplot2
-  # ggsci package: https://cran.r-project.org/web/packages/ggsci/vignettes/ggsci.html
-  ggscipal <- c("npg", "aaas", "lancet", "jco",
-                "ucscgb", "uchicago", "simpsons", "rickandmorty")
+  brewerpal <- .brewerpal()
+  ggscipal <- .ggscipal()
 
   res <- NULL
   if (is.null(palette))
@@ -265,6 +203,54 @@ NULL
   functs[[pal]]
 }
 
+# Generate color palette from ggsci or Rcolorbrewer
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+.get_pal <- function(pal, k){
+ if(pal %in% .brewerpal()) .get_brewer_pal(pal, k)
+  else if(pal %in% .ggscipal()) .get_ggsci_pal(pal, k)
+}
+
+# Generate color palette from ggsci
+# k the number of color
+.get_ggsci_pal <- function(palette = c("npg", "aaas", "lancet", "jco",
+                                           "ucscgb", "uchicago", "simpsons", "rickandmorty"), k)
+{
+
+  pal <- match.arg(palette)
+
+  functs <- list(
+    npg = ggsci::pal_npg(),
+    aaas = ggsci::pal_aaas(),
+    lancet = ggsci::pal_lancet(),
+    jco = ggsci::pal_jco(),
+    ucscgb = ggsci::pal_ucscgb(),
+    uchicago = ggsci::pal_uchicago(),
+    simpsons = ggsci::pal_simpsons(),
+    rickandmorty = ggsci::pal_rickandmorty()
+  )
+  functs[[pal]](k)
+}
+
+# Generate a color palette from brewer
+.get_brewer_pal <- function(palette, k){
+  if (!requireNamespace("RColorBrewer", quietly = TRUE)) {
+    stop("RColorBrewer package needed. Please install it using install.packages('RColorBrewer').")
+  }
+  pal <- palette[1]
+  sequential <- c('Blues', 'BuGn', 'BuPu', 'GnBu', 'Greens', 'Greys', 'Oranges',
+    'OrRd', 'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu', 'Reds','YlGn', 'YlGnBu YlOrBr', 'YlOrRd')
+  divergent <- c('BrBG', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral')
+
+  if(pal %in% sequential) max_k <- 9
+  else if(pal %in% divergent) max_k <- 11
+  else if(pal %in% c('Accent','Dark2','Pastel2', 'Set2')) max_k <- 8
+  else if(pal %in% c('Pastel1',  'Set1')) max_k <- 9
+  else if(pal %in% c('Paired', 'Set3')) max_k <- 12
+  else stop("Don't support palette name: ", pal)
+
+  if(k <= max_k) RColorBrewer::brewer.pal(k, palette)
+  else grDevices::colorRampPalette(RColorBrewer::brewer.pal(max_k, palette))(k)
+}
 
 # Set plot orientation
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
