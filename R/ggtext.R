8aaa8f363687f7c8779ccad3b9f6e7b6b40abd01
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-05-08 07:40:18 +0200

    new arguments added

--- R/ggtext.R
+++ R/ggtext.R
@@ -3,23 +3,22 @@ NULL
 #' Text
 #' @description Add text to a plot.
 #' @inheritParams ggscatter
+#' @param data a data frame
 #' @param x,y x and y variables for drawing.
-#' @param color point colors.
 #' @param label the name of the column containing point labels. Can be also a
 #'   character vector with length = nrow(data).
-#' @param font.label a vector of length 3 indicating respectively the size
-#'   (e.g.: 14), the style (e.g.: "plain", "bold", "italic", "bold.italic") and
-#'   the color (e.g.: "red") of point labels. For example \emph{font.label =
-#'   c(14, "bold", "red")}. To specify only the size and the style, use
-#'   font.label = c(14, "plain").
+#' @param color text font color.
+#' @param size text font size.
+#' @param face text font style. Allowed values are one of c("plain", "bold",
+#'   "italic", "bold.italic").
+#' @param family character vector specifying font family.
 #' @param label.select character vector specifying some labels to show.
 #' @param repel a logical value, whether to use ggrepel to avoid overplotting
 #'   text labels or not.
 #' @param label.rectangle logical value. If TRUE, add rectangle underneath the
 #'   text, making it easier to read.
 #' @param ggp a ggplot. If not NULL, points are added to an existing plot.
-#' @param ... other arguments to be passed to \code{\link[ggplot2]{geom_point}}
-#'   and \code{\link{ggpar}}.
+#' @param ... other arguments to be passed to \code{\link{ggpar}}.
 #' @details The plot can be easily customized using the function ggpar(). Read
 #'   ?ggpar for changing: \itemize{ \item main title and axis labels: main,
 #'   xlab, ylab \item axis limits: xlim, ylim (e.g.: ylim = c(0, 30)) \item axis
@@ -48,12 +47,11 @@ NULL
 #'
 #'
 #' @export
-ggtext <- function(data, x, y,
-                      color = "black",  palette = NULL,
-                      size = 2, point = TRUE,
-                      label = NULL,  font.label = c(12, "plain"),
-                      label.select = NULL, repel = FALSE, label.rectangle = FALSE,
-                      ggp = NULL, ggtheme = theme_classic(),
+ggtext <- function(data, x, y, label = NULL,
+                  color = "black",  palette = NULL,
+                  size = 12,  face = "plain", family = "",
+                  label.select = NULL, repel = FALSE, label.rectangle = FALSE,
+                  ggp = NULL, ggtheme = theme_pubr(),
                       ...)
 {
 
@@ -64,12 +62,6 @@ ggtext <- function(data, x, y,
     else data$label.xx <- label
     label <- "label.xx"
   }
-
-  # label font
-  font.label <- .parse_font(font.label)
-  font.label$size <- ifelse(is.null(font.label$size), 12, font.label$size)
-  font.label$color <- ifelse(is.null(font.label$color), color, font.label$color)
-  font.label$face <- ifelse(is.null(font.label$face), "plain", font.label$face)
 
   if(is.null(ggp)) p <- ggplot(data, aes_string(x, y))
   else p <- ggp
@@ -89,9 +81,10 @@ ggtext <- function(data, x, y,
     if(repel){
       ggfunc <- ggrepel::geom_text_repel
       if(label.rectangle) ggfunc <- ggrepel::geom_label_repel
-        p <- p + .geom_exec(ggfunc, data = lab_data, x = x, y = y,
-                          label = label, fontface = font.label$face,
-                          size = font.label$size/3, color = font.label$color,
+        p <- p + geom_exec(ggfunc, data = lab_data, x = x, y = y,
+                          label = label, fontface = face,
+                          family = family,
+                          size = size/3, color = color,
                           alpha = alpha,
                           box.padding = unit(0.35, "lines"),
                           point.padding = unit(0.3, "lines"),
@@ -104,13 +97,15 @@ ggtext <- function(data, x, y,
         ggfunc <- geom_label
         vjust <- -0.4
         }
-      p <- p + .geom_exec(ggfunc, data = lab_data, x = x, y = y, color = color,
-                          label = label, fontface = font.label$face,
-                          size = font.label$size/3, color = font.label$color,
+      p <- p + geom_exec(ggfunc, data = lab_data, x = x, y = y, color = color,
+                          label = label, fontface = face, family = family,
+                          size = size/3, color = color,
                           vjust = vjust, alpha = alpha)
     }
   }
   p <- ggpar(p, palette = palette, ggtheme = ggtheme, ...)
+  if(family != "")
+    p <- p + theme(text = element_text(family = family))
   p
 }
 
