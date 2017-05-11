81359c0de1fc38e0aac4f4f71872a002ffa66939
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-05-11 07:23:05 +0200

    new arguments added in theme_pubr: border, margin, legend, x.text.angle

--- R/theme_pubr.R
+++ R/theme_pubr.R
@@ -1,16 +1,26 @@
 #' @include utilities.R
 #' @import ggplot2
 NULL
-#' Publication ready theme
+#'Publication ready theme
 #'
-#' @description
-#' \itemize{
-#'  \item \strong{theme_pubr()}: Create a publication ready theme
-#'  \item \strong{labs_pubr()}: Format only plot labels to a publication ready style
-#'  \item \strong{theme_classic()}: Create a classic theme with axis lines
-#' }
-#' @param base_size base font size
-#' @param base_family base font family
+#'@description \itemize{ \item \strong{theme_pubr()}: Create a publication ready
+#'  theme \item \strong{labs_pubr()}: Format only plot labels to a publication
+#'  ready style \item \strong{theme_classic2()}: Create a classic theme with
+#'  axis lines }
+#'@param base_size base font size
+#'@param base_family base font family
+#'@param border logical value. Default is FALSE. If TRUE, add panel border.
+#'@param margin logical value. Default is TRUE. If FALSE, reduce plot margin.
+#'@param legend character specifying legend position. Allowed values are one of
+#'  c("top", "bottom", "left", "right", "none"). Default is "top" side position.
+#'  to remove the legend use legend = "none". Legend position can be also
+#'  specified using a numeric vector c(x, y).  In this case it is possible to
+#'  position the legend inside the plotting area. x and y are the coordinates of
+#'  the legend box. Their values should be between 0 and 1. c(0,0) corresponds
+#'  to the "bottom left" and c(1,1) corresponds to the "top right" position. For
+#'  instance use legend = c(0.8, 0.2).
+#'@param x.text.angle Rotation angle of x axis tick labels.
+#'  Default value is 0. Use 90 for vertical text.
 #' @examples
 #' p <- ggplot(mtcars, aes(x = wt, y = mpg)) +
 #'    geom_point(aes(color = gear))
@@ -24,38 +34,50 @@ NULL
 #' # Format labels
 #' p + labs_pubr()
 #'
-#' @name theme_pubr
-#' @rdname theme_pubr
-#' @export
-theme_pubr <-
-  function (base_size = 14, base_family = "")
-  {
-    theme_bw(base_size = base_size, base_family = base_family) %+replace%
-      theme(
-        panel.border = element_blank(), axis.line = element_line(colour = "black"),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        strip.background = element_rect(colour = "black",
-                                        size = 0.5),
-        legend.key = element_blank(),
-        # Tick labels
-        axis.text.x = element_text(size = rel(0.86), colour = "black",face = "bold"),
-        axis.text.y = element_text(size = rel(0.86), colour = "black",face = "bold"),
+#'@name theme_pubr
+#'@rdname theme_pubr
+#'@export
+theme_pubr <- function (base_size = 12, base_family = "",
+                        border = FALSE, margin = TRUE,
+                        legend = c("top", "bottom", "left", "right", "none"),
+                        x.text.angle = 0)
+{
+  half_line <- base_size/2
+  if(!is.numeric(legend)) legend <- match.arg(legend)
+  if(x.text.angle > 5) xhjust <- 1 else xhjust <- NULL
 
-        # Axis
-        axis.title = element_text(size = rel(1), colour = "black", face = "bold"),
-        axis.line.x = element_line(colour = "black", size = 1),
-        axis.line.y = element_line(colour = "black", size = 1),
-        axis.ticks = element_line(colour = "black", size = 1),
-
-        # Main title
-        plot.title = element_text(size = rel(1), colour = "black" ,
-                                  lineheight=1.0, face = "bold"),
-
-        legend.position = "bottom",
-        legend.title = element_text(size = rel(0.7), face = "bold", colour = "black"),
-        legend.text = element_text(size = rel(0.7), face = "plain", colour = "black")
-      )
+  if(border){
+    panel.border <- element_rect(fill = NA, colour = "black", size = 0.7)
+    axis.line <- element_blank()
   }
+  else{
+    panel.border <- element_blank()
+    axis.line = element_line(colour = "black", size = 0.5)
+  }
+
+
+  if(margin)
+    plot.margin <- margin(half_line, half_line, half_line,
+                          half_line)
+  else plot.margin <- unit(c(0.5,0.3,0.3,0.3),"mm")
+
+  .theme <- theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(panel.border = panel.border,
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.line = axis.line,
+          legend.key = element_blank(),
+          strip.background = element_rect(fill = "#F2F2F2", colour = "black", size = 0.7),
+          plot.margin = plot.margin,
+          legend.position = legend,
+          complete = TRUE)
+
+  if(x.text.angle!=0)
+    .theme <- .theme + theme(axis.text.x = element_text(angle = x.text.angle, hjust = xhjust))
+
+  .theme
+}
+
 
 #' @rdname theme_pubr
 #' @export
@@ -91,3 +113,5 @@ theme_classic2 <-
         axis.line.y = element_line()
       )
   }
+
+
