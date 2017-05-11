64f9d7b0eba772ee0eda3a53197fe62c5b8df400
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-05-11 07:06:45 +0200

    new arguments grouping.vars added

--- R/ggtext.R
+++ R/ggtext.R
@@ -12,11 +12,22 @@ NULL
 #' @param face text font style. Allowed values are one of c("plain", "bold",
 #'   "italic", "bold.italic").
 #' @param family character vector specifying font family.
-#' @param label.select character vector specifying some labels to show.
+#' @param label.select can be of two formats: \itemize{
+#'   \item a character vector specifying some labels to show.
+#'   \item a list containting one or the combination of the following components:
+#'     \itemize{
+#'        \item \code{top.up} and \code{top.down}: to display the labels  of the top up/down points.
+#'        For example, \code{label.select = list(top.up = 10, top.down = 4)}.
+#'        \item \code{criteria}: to filter, for example, by x and y variabes values,
+#'        use this: \code{label.select = list(criteria = list("`y` > 2 & `y` < 5 & `x` \%in\% c('A', 'B')"))}.
+#'   }
+#'   }
 #' @param repel a logical value, whether to use ggrepel to avoid overplotting
 #'   text labels or not.
 #' @param label.rectangle logical value. If TRUE, add rectangle underneath the
 #'   text, making it easier to read.
+#' @param grouping.vars grouping variables to sort the data by, when the user
+#'   wants to display the top n up/down labels.
 #' @param ggp a ggplot. If not NULL, points are added to an existing plot.
 #' @param ... other arguments to be passed to \code{\link{ggpar}}.
 #' @details The plot can be easily customized using the function ggpar(). Read
@@ -51,6 +62,7 @@ ggtext <- function(data, x = NULL, y = NULL, label = NULL,
                   color = "black",  palette = NULL,
                   size = 11,  face = "plain", family = "",
                   label.select = NULL, repel = FALSE, label.rectangle = FALSE,
+                  grouping.vars = NULL,
                   ggp = NULL, ggtheme = theme_pubr(),
                       ...)
 {
@@ -72,43 +84,108 @@ ggtext <- function(data, x = NULL, y = NULL, label = NULL,
   alpha <- 1
   if(!is.null(list(...)$alpha)) alpha <- list(...)$alpha
 
-  if(!is.null(label)) {
-    lab_data <- data
-    # Select some labels to show
-    if(!is.null(label.select))
-      lab_data  <- subset(lab_data, lab_data[, label, drop = TRUE] %in% label.select,
-                          drop = FALSE)
+  if(is.null(label))
+    return(p)
 
-    if(repel){
-      set.seed(123)
-      ggfunc <- ggrepel::geom_text_repel
-      if(label.rectangle) ggfunc <- ggrepel::geom_label_repel
-        p <- p + geom_exec(ggfunc, data = lab_data, x = x, y = y,
-                          label = label, fontface = face,
-                          family = family,
-                          size = size/3, color = color,
-                          alpha = alpha,
-                          box.padding = unit(0.35, "lines"),
-                          point.padding = unit(0.3, "lines"),
-                          force = 1)
-    }
-    else{
-      ggfunc <- geom_text
-      vjust  <- -0.7
-      if(label.rectangle) {
-        ggfunc <- geom_label
-        vjust <- -0.4
-        }
-      p <- p + geom_exec(ggfunc, data = lab_data, x = x, y = y, color = color,
-                          label = label, fontface = face, family = family,
-                          size = size/3, color = color,
-                          vjust = vjust, alpha = alpha)
-    }
+  lab_data <- data
+  # Select some labels to show
+  if(!is.null(label.select)){
+    lab_data <- .get_label_data (data, x, y, label = label,
+                                label.select = label.select,
+                                grouping.vars = grouping.vars)
   }
+
+  if(repel){
+    set.seed(123)
+    ggfunc <- ggrepel::geom_text_repel
+    if(label.rectangle) ggfunc <- ggrepel::geom_label_repel
+      p <- p + geom_exec(ggfunc, data = lab_data, x = x, y = y,
+                        label = label, fontface = face,
+                        family = family,
+                        size = size/3, color = color,
+                        alpha = alpha,
+                        box.padding = unit(0.35, "lines"),
+                        point.padding = unit(0.3, "lines"),
+                        force = 1)
+  }
+  else{
+    ggfunc <- geom_text
+    vjust  <- -0.7
+    if(label.rectangle) {
+      ggfunc <- geom_label
+      vjust <- -0.4
+    }
+    p <- p + geom_exec(ggfunc, data = lab_data, x = x, y = y, color = color,
+                        label = label, fontface = face, family = family,
+                        size = size/3, color = color,
+                        vjust = vjust, alpha = alpha)
+
+  }
+
   p <- ggpar(p, palette = palette, ggtheme = ggtheme, ...)
   if(family != "")
     p <- p + theme(text = element_text(family = family))
+
   p
+}
+
+
+
+# data: data frame
+# x, y: x and y variables
+# label: label columns
+# label.select: select some labels. Can be a character vector, or a list
+#   with the following components (top.up, top.down)
+# grouping.vars grouping variables
+.get_label_data <- function(data, x, y, label = NULL,
+                            label.select = NULL, grouping.vars = NULL)
+
+  {
+
+  if(.is_list(label.select)){
+    expected.components = c("top.up", "top.down")
+    if(!any(expected.components %in% names(label.select)))
+      stop("If label.select is a list, it should contain one or the combination ",
+           "of the following element: ", .collapse(expected.components, sep = ", "))
+  }
+
+  data <- as.data.frame(data)
+
+  if(is.null(label))
+    lab_data <- NULL
+
+  else if(is.null(label.select))
+    lab_data <- data
+
+  else if(.is_list(label.select)){
+
+    lab_data <- data
+    top_up <- top_down <- . <- NULL
+
+    if(!is.null(label.select$top.up))
+      top_up <- .top_up(data, x, y, n = label.select$top.up,
+                        grouping.vars = grouping.vars)
+
+    if(!is.null(label.select$top.down))
+      top_down <- .top_down(data, x, y, n = label.select$top.down,
+                            grouping.vars = grouping.vars)
+
+    if(!is.null(top_up) | !is.null(top_down))
+      lab_data <- rbind(top_up, top_down)
+
+    if(!is.null(label.select$criteria)){
+      criteria <- gsub("`y`", y, label.select$criteria) %>%
+        gsub("`x`", x, .)
+      lab_data <- dplyr::filter_(lab_data, .dots = criteria)
+    }
+
+
+  }
+
+  else lab_data  <- subset(data, data[, label, drop = TRUE] %in% label.select,
+                        drop = FALSE)
+
+  return(lab_data)
 }
 
 
