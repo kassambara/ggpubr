33d918a7391d61d93c1d599a3d6b3a50d78e28e7
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-05-16 08:49:26 +0200

    doc updated

--- R/gene_citation.R
+++ R/gene_citation.R
@@ -12,6 +12,11 @@
 #' @examples
 #' data(gene_citation)
 #'
+#'# Some key genes of interest to be highlighted
+#'key.gns <- c("MYC", "PRDM1", "CD69", "IRF4", "CASP3", "BCL2L1", "MYB",  "BACH2", "BIM1",  "PTEN",
+#'             "KRAS", "FOXP1", "IGF1R", "KLF4", "CDK6", "CCND2", "IGF1", "TNFAIP3", "SMAD3", "SMAD7",
+#'             "BMPR2", "RB1", "IGF2R", "ARNT")
+#'# Density distribution
 #'ggdensity(gene_citation, x = "citation_index", y = "..count..",
 #'   xlab = "Number of citation",
 #'   ylab = "Number of genes",
