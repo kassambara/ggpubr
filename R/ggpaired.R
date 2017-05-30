0d628afe6fae855732ef4f661c2e9a53ef2cc729
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-05-30 06:17:58 +0200

    new arguments x and y

--- R/ggpaired.R
+++ R/ggpaired.R
@@ -5,14 +5,21 @@ NULL
 #'@inheritParams ggboxplot
 #'@param cond1 variable name corresponding to the first condition.
 #'@param cond2 variable name corresponding to the second condition.
-#'@param color points and boxs plot colors. To color by conditions, use color = "condition".
-#'@param fill box plot fill color. To change fill color by conditions, use fill = "condition".
+#'@param x,y x and y variables, where x is a grouping variable and y contains
+#'  values for each group. Considered only when \code{cond1} and \code{cond2}
+#'  are missing.
+#'@param color points and boxs plot colors. To color by conditions, use color =
+#'  "condition".
+#'@param fill box plot fill color. To change fill color by conditions, use fill
+#'  = "condition".
 #'@param line.color line color.
 #'@param point.size,line.size point and line size, respectively.
 #'@param width box plot width.
 #'@param ... other arguments to be passed to be passed to \link{ggpar}().
 #' @examples
 #'
+#'# Example 1
+#'#::::::::::::::::::::::::::::::::::::::::::
 #' before <-c(200.1, 190.9, 192.7, 213, 241.4, 196.9, 172.2, 185.5, 205.2, 193.7)
 #' after <-c(392.9, 393.2, 345.1, 393, 434, 427.9, 422, 383.9, 392.3, 352.2)
 #'
@@ -20,9 +27,14 @@ NULL
 #' ggpaired(d, cond1 = "before", cond2 = "after",
 #'     fill = "condition", palette = "jco")
 #'
+#'# Example 2
+#'#::::::::::::::::::::::::::::::::::::::::::
+#'ggpaired(ToothGrowth, x = "supp", y = "len",
+#'  color = "supp", line.color = "gray", line.size = 0.4,
+#'  palette = "npg")
 #'
-#' @export
-ggpaired <- function(data, cond1, cond2,
+#'@export
+ggpaired <- function(data, cond1, cond2, x = NULL, y = NULL,
                         color = "black", fill = "white", palette = NULL,
                         width = 0.5, point.size = 1.2, line.size = 0.5, line.color = "black",
                         title = NULL, xlab = "Condition", ylab = "Value",
@@ -30,15 +42,23 @@ ggpaired <- function(data, cond1, cond2,
                         ...)
 {
 
+  if(!missing(cond1) & !missing(cond2)){
   data <- data %>%
     tidyr::gather_(key_col = "condition", value_col = "val",
                    gather_cols = c(cond1, cond2)) %>%
     dplyr::mutate(id = rep(1:nrow(data), 2))
-
   data$condition<- factor(data$condition, levels = c(cond1, cond2))
+  x <- "condition"
+  y <- "val"
+  }
+  else if(!is.null(x) & !is.null(y)){
+    data$id <-  rep(1:(nrow(data)/2), 2)
+    if(missing(xlab)) xlab <- x
+    if(missing(ylab)) ylab <- y
+  }
 
   condition <- val <- id <- NULL
-  p <- ggplot(data, aes(x = condition, y = val)) +
+  p <- ggplot(data, aes_string(x = x, y = y)) +
     geom_exec(geom_boxplot, data = data, color = color, fill = fill, width = width)+
     geom_line(aes(group= id),  color = line.color, size = line.size)+
     geom_exec(geom_point, data = data, color = color, size = point.size)
@@ -47,4 +67,3 @@ ggpaired <- function(data, cond1, cond2,
 
   p
 }
-
