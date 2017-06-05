c6b8dfe8ae16df641d971bbbb1da07fe7749ecfe
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-06-05 20:52:18 +0200

    argument names updated

--- R/font.R
+++ R/font.R
@@ -8,12 +8,12 @@
 #'  include: \itemize{ \item \code{"title"} for the main title \item
 #'  \code{"subtitle"} for the plot subtitle \item \code{"caption"} for the plot
 #'  caption \item \code{"legend.title"} for the legend title \item
-#'  \code{"legend.text"} for the legend text \item \code{"x", "xlab", or "xtitle"}
-#'  for x axis label \item \code{"y", "ylab", or "ytitle"} for y axis label \item
-#'  \code{"xy", "xylab", "xytitle" or "axistitle"} for both x and y axis
-#'  labels \item \code{"xtext"} for x axis texts (x axis tick labels) \item
-#'  \code{"ytext"} for y axis texts (y axis tick labels) \item \code{"xytext"}
-#'  or \code{"axistext"} for both x and y axis texts }
+#'  \code{"legend.text"} for the legend text \item \code{"x", "xlab", or "x.title"}
+#'  for x axis label \item \code{"y", "ylab", or "y.title"} for y axis label \item
+#'  \code{"xy", "xylab", "xy.title" or "axis.title"} for both x and y axis
+#'  labels \item \code{"x.text"} for x axis texts (x axis tick labels) \item
+#'  \code{"y.text"} for y axis texts (y axis tick labels) \item \code{"xy.text"}
+#'  or \code{"axis.text"} for both x and y axis texts }
 #'
 #'@param size numeric value specifying the font size, (e.g.: \code{size = 12}).
 #'@param color character string specifying the font color, (e.g.: \code{color =
@@ -45,7 +45,7 @@
 #'  font("caption", size = 10, color = "orange")+
 #'  font("xlab", size = 12, color = "blue")+
 #'  font("ylab", size = 12, color = "#993333")+
-#'  font("xytext", size = 12, color = "gray", face = "bold")
+#'  font("xy.text", size = 12, color = "gray", face = "bold")
 #'
 #'# Change the appearance of legend title and texts
 #'p +
@@ -64,16 +64,16 @@ font <- function(object, size = NULL, color = NULL, face = NULL, family = NULL, 
 
          x = theme(axis.title.x = elmt),
          xlab = theme(axis.title.x = elmt),
-         xtitle = theme(axis.title.x = elmt),
+         x.title = theme(axis.title.x = elmt),
 
          y = theme(axis.title.y = elmt),
          ylab = theme(axis.title.y = elmt),
-         ytitle = theme(axis.title.y = elmt),
+         y.title = theme(axis.title.y = elmt),
 
          xy = theme(axis.title.x = elmt, axis.title.y = elmt),
          xylab = theme(axis.title.x = elmt, axis.title.y = elmt),
-         xytitle = theme(axis.title.x = elmt, axis.title.y = elmt),
-         axistitle = theme(axis.title.x = elmt, axis.title.y = elmt),
+         xy.title = theme(axis.title.x = elmt, axis.title.y = elmt),
+         axis.title = theme(axis.title.x = elmt, axis.title.y = elmt),
 
          legendtitle = theme(legend.title  = elmt),
          legend.title = theme(legend.title  = elmt),
@@ -81,11 +81,11 @@ font <- function(object, size = NULL, color = NULL, face = NULL, family = NULL, 
          legend.text = theme(legend.text  = elmt),
 
          # Axis tick labels
-         xtext = theme(axis.text.x  = elmt),
-         ytext = theme(axis.text.y  = elmt),
-         xytext = theme(axis.text.x  = elmt, axis.text.y  = elmt),
+         x.text = theme(axis.text.x  = elmt),
+         y.text = theme(axis.text.y  = elmt),
+         xy.text = theme(axis.text.x  = elmt, axis.text.y  = elmt),
          yxtext = theme(axis.text.x  = elmt, axis.text.y  = elmt),
-         axistext = theme(axis.text.x  = elmt, axis.text.y  = elmt)
-
+         axis.text = theme(axis.text.x  = elmt, axis.text.y  = elmt),
+         stop("Don't support ", object)
          )
 }
