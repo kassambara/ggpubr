bc8cece31c0d97cb97c5358512993e97484b0746
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-10-30 11:55:46 +0100

    doc updated for plotting multiple groups #45

--- R/ggdotchart.R
+++ R/ggdotchart.R
@@ -52,6 +52,24 @@ NULL
 #'    y.text.col = TRUE )
 #'
 #'
+#'# Plot with multiple groups
+#'# +++++++++++++++++++++
+#'# Create some data
+#'df2 <- data.frame(supp=rep(c("VC", "OJ"), each=3),
+#'                  dose=rep(c("D0.5", "D1", "D2"),2),
+#'                  len=c(6.8, 15, 33, 4.2, 10, 29.5))
+#'print(df2)
+#'
+#'ggdotchart(df2, x = "dose", y = "len",
+#'           color = "supp", size = 3,
+#'           add = "segment",
+#'           add.params = list(color = "lightgray", size = 1.5),
+#'           position = position_dodge(0.3),
+#'           palette = "jco",
+#'           ggtheme = theme_pubclean()
+#')
+#'
+#'
 #' @export
 ggdotchart <- function(data, x, y, group = NULL,
                        combine = FALSE,
