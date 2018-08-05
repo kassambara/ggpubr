68961f01afe44fb3e2e86d877cbdd9d651e2e2a1
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2018-08-05 16:48:56 +0200

    Examples updated

--- R/stat_pvalue_manual.R
+++ R/stat_pvalue_manual.R
@@ -30,21 +30,34 @@ NULL
 #'@seealso \code{\link{stat_compare_means}}
 #'@examples
 #'
-#' require("dplyr")
+#'# T-test
+#'stat.test <- compare_means(
+#'  len ~ dose, data = ToothGrowth,
+#'  method = "t.test"
+#')
+#'stat.test
 #'
-#' #:::::::::::::::::::::::::::::::::::
-#' # Simple box plot
-#' #:::::::::::::::::::::::::::::::::::
-#' # Pairwise t-test between groups
-#' stat.test <- compare_means(
-#'   len ~ dose, data = ToothGrowth,
-#'   method = "t.test"
-#' ) %>%
-#'   mutate(y.position = c(29, 35, 39))
-#' stat.test
-#' # Create a box plot and add the p-value
-#' ggboxplot(ToothGrowth, x = "dose", y = "len") +
-#'   stat_pvalue_manual(stat.test, label = "p.adj")
+#'# Create a simple box plot
+#'p <- ggboxplot(ToothGrowth, x = "dose", y = "len")
+#'p
+#'
+#'# Perform a t-test between groups
+#'stat.test <- compare_means(
+#'  len ~ dose, data = ToothGrowth,
+#'  method = "t.test"
+#')
+#'stat.test
+#'
+#'# Add manually p-values from stat.test data
+#'# First specify the y.position of each comparison
+#'stat.test <- stat.test %>%
+#'  mutate(y.position = c(29, 35, 39))
+#'p + stat_pvalue_manual(stat.test, label = "p.adj")
+#'
+#'# Customize the label with glue expression
+#'# (https://github.com/tidyverse/glue)
+#'p + stat_pvalue_manual(stat.test, label = "p = {p.adj}")
+#'
 #'@export
 stat_pvalue_manual <- function(
   data, label = "p", y.position = "y.position",
