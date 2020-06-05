1f8a505d505f8381cfd201cdd57f2bbfdb6ef4db
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2020-06-06 01:03:23 +0200

    New function create_aes() added #229

---
+++ R/create_aes.R
@@ -0,0 +1,47 @@
+#' @include utilities.R
+NULL
+#' Create Aes Mapping from a List
+#'
+#' @description Create aes mapping to make programming easy with ggplot2.
+#' @param .list a list of aesthetic arguments; for example .list = list(x = "dose", y = "len", color = "dose").
+#' @param parse logical. If TRUE, parse the input as an expression.
+#'
+#' @examples
+#' # Simple aes creation
+#' create_aes(list(x = "Sepal.Length", y = "Petal.Length" ))
+#'
+#' # Parse an expression
+#' x <- "log2(Sepal.Length)"
+#' y <- "log2(Petal.Length)"
+#' create_aes(list(x = x, y = y ), parse = TRUE)
+#'
+#' # Create a ggplot
+#' mapping <- create_aes(list(x = x, y = y ), parse = TRUE)
+#' ggplot(iris, mapping) +
+#'  geom_point()
+#'
+#' @export
+create_aes <- function(.list, parse = FALSE){
+  if(parse){
+    return(create_aes.parse(.list))
+  } else{
+    return(create_aes.name(.list))
+  }
+}
+
+
+# Ref:  https://rpubs.com/hadley/97970
+# Parse as name. If x_var is "a + b", as.name() will turn it into a variable called `a + b`.
+create_aes.name <- function(.list){
+  .list <- .list %>%
+    purrr::map(function(.list) as.name(.list))
+  do.call(ggplot2::aes, .list)
+}
+
+# Parse an expression. If x_var is "a + b", parse() will turn it into the function call a + b
+create_aes.parse <- function(.list){
+  .list <- .list %>% purrr:: map(function(x) parse(text = x)[[1]])
+  do.call(ggplot2::aes, .list)
+}
+
+
