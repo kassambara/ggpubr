32eb9c842e204abe1d4245f4ae1a107d79d606f4
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2025-06-24 21:06:57 +0000

    Fixed compatibility with ggplot2 4.0.0. Updated  tests #635

--- tests/testthat/test-gghistogram.R
+++ tests/testthat/test-gghistogram.R
@@ -5,7 +5,11 @@ test_that("gghistogram works when using after_stat()", {
   p_build <- ggplot2::ggplot_build(p)
   df <- p_build$data[[1]]
   observed_count <- df$count
-  expected_count <- c(4, 1, 4, 2, 11, 10, 9, 4, 7, 7, 6, 8, 7, 9, 6, 4, 9, 12, 2, 8, 3, 5, 1, 3, 1, 1, 1, 4, 0, 1)
+  if (is_pkg_version_sup("ggplot2", "3.5.2")) {
+    expected_count <- c(1, 3, 5, 2, 5, 6, 19, 4, 1, 6, 13, 8, 7, 3, 12, 4, 9, 7, 5, 10, 3, 4, 1, 4, 1, 1, 0, 5, 0, 1)
+  } else {
+    expected_count <- c(4, 1, 4, 2, 11, 10, 9, 4, 7, 7, 6, 8, 7, 9, 6, 4, 9, 12, 2, 8, 3, 5, 1, 3, 1, 1, 1, 4, 0, 1)
+  }
   expect_equal(observed_count, expected_count)
 })
 
@@ -15,6 +19,10 @@ test_that("gghistogram works when using after_stat() with trailing space inside 
   p_build <- ggplot2::ggplot_build(p)
   df <- p_build$data[[1]]
   observed_count <- df$count
-  expected_count <- c(4, 1, 4, 2, 11, 10, 9, 4, 7, 7, 6, 8, 7, 9, 6, 4, 9, 12, 2, 8, 3, 5, 1, 3, 1, 1, 1, 4, 0, 1)
+  if (is_pkg_version_sup("ggplot2", "3.5.2")) {
+    expected_count <- c(1, 3, 5, 2, 5, 6, 19, 4, 1, 6, 13, 8, 7, 3, 12, 4, 9, 7, 5, 10, 3, 4, 1, 4, 1, 1, 0, 5, 0, 1)
+  } else {
+    expected_count <- c(4, 1, 4, 2, 11, 10, 9, 4, 7, 7, 6, 8, 7, 9, 6, 4, 9, 12, 2, 8, 3, 5, 1, 3, 1, 1, 1, 4, 0, 1)
+  }
   expect_equal(observed_count, expected_count)
 })
